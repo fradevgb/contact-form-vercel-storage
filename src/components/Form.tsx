9fd0d3528b5a800384fb358ae9e0d54eba1a83de
Frank Gaspar <frank.gasparfcytsis@gmail.com>
2023-05-08 10:40:41 -0400

    Add contact form to KV

---
+++ src/components/Form.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import { Button } from "@/components/Button"
+import { Input } from "@/components/Input"
+import { toast } from "sonner"
+import confetti from "canvas-confetti"
+
+export const Form = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const form = event.currentTarget
+    const formData = new FormData(form)
+    // formData.get('email')
+    const { email, name, message } = Object.fromEntries(formData.entries())
+
+    // a llamar a la API
+    fetch('/api/kv-send-message', {
+      method: 'POST',
+      body: JSON.stringify({ email, name, message }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(() => {
+      toast.success('Mensaje enviado con exito')
+      form.reset()
+      confetti()
+    }).catch(() => {
+      toast.error('Hubo un error al enviar el mensaje')
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className=" max-w-[400px] p-8 m-auto space-y-8 border rounded border-white/10">
+      <Input
+        name="email"
+        id="email"
+        label="Tu Email:"
+        type="email"
+        placeholder="example@email.com"
+      />
+      <Input
+        name="name"
+        id="name"
+        label="Tu Nombre:"
+        type="text"
+        placeholder="John Doe"
+      />
+      <Input
+        name="message"
+        id="message"
+        label="Tu Mensaje:"
+        type="text"
+        placeholder="Este es el mensaje que quiero enviar"
+      />
+      <Button>Enviar mensaje</Button>
+    </form>
+  )
+}
---
+++ src/components/Toaster.tsx
@@ -0,0 +1,8 @@
+'use client'
+import { Toaster as ToasterComponent } from "sonner"
+
+export const Toaster = () => {
+  return (
+    <ToasterComponent  position="top-center"/>
+  )
+}
